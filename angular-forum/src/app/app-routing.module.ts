b190e84f2558c4fe957b5cc2565d66af4f95da9a
Kevin Nunes da Silva <kmh.dasilva@gmail.com>
2021-11-27 23:06:21 +0100

    post click shows detail page

--- angular-forum/src/app/app-routing.module.ts
+++ angular-forum/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomepageComponent } from './homepage/homepage.component';
+import { PostDetailComponent } from './post-detail/post-detail.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/homepage', pathMatch: 'full' },
-  { path: 'homepage', component: HomepageComponent }
+  { path: 'homepage', component: HomepageComponent },
+  { path: 'post/:id', component: PostDetailComponent }
 ];
 
 @NgModule({
