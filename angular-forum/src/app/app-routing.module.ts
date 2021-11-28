b9914a826167b5375cd2e56c9901efb5d4b1181c
Kevin Nunes da Silva <kmh.dasilva@gmail.com>
2021-11-28 18:16:10 +0100

    showing posts of same category in category-list component

--- angular-forum/src/app/app-routing.module.ts
+++ angular-forum/src/app/app-routing.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CategoryListComponent } from './category-list/category-list.component';
 import { HomepageComponent } from './homepage/homepage.component';
 import { PostDetailComponent } from './post-detail/post-detail.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/homepage', pathMatch: 'full' },
   { path: 'homepage', component: HomepageComponent },
-  { path: 'post/:id', component: PostDetailComponent }
+  { path: 'post/:id', component: PostDetailComponent },
+  {path: 'categories', component: CategoryListComponent},
+  {path: 'category/:id', component: CategoryListComponent}
 ];
 
 @NgModule({
--- angular-forum/src/app/in-memory-data.service.ts
+++ angular-forum/src/app/in-memory-data.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { InMemoryDbService } from 'angular-in-memory-web-api';
 import { Post } from './post';
+import {Category} from './category';
 
 @Injectable({
   providedIn: 'root',
@@ -8,21 +9,29 @@ import { Post } from './post';
 export class InMemoryDataService implements InMemoryDbService {
   createDb() {
     const posts = [
-      { id: 1, name: 'post 1', description: 'asdasdasfhgdhsfadfgkvnrgekatgch', category: "1" },
-      { id: 2, name: 'post 2', description: 'asdasdasfhgdhsfadfgkvnrgekatgch', category: "1"  },
-      { id: 3, name: 'post 3', description: 'asdasdasfhgdhsfadfgkvnrgekatgch', category: "2"  },
-      { id: 4, name: 'post 4', description: 'asdasdasfhgdhsfadfgkvnrgekatgch', category: "2"  },
-      { id: 5, name: 'post 5', description: 'asdasdasfhgdhsfadfgkvnrgekatgch', category: "3"  },
-      { id: 6, name: 'post 6', description: 'asdasdasfhgdhsfadfgkvnrgekatgch', category: "4"  }
+      { id: 1, name: 'post 1', description: 'asdasdasfhgdhsfadfgkvnrgekatgch', category: 1 },
+      { id: 2, name: 'post 2', description: 'asdasdasfhgdhsfadfgkvnrgekatgch', category: 1  },
+      { id: 3, name: 'post 3', description: 'asdasdasfhgdhsfadfgkvnrgekatgch', category: 2  },
+      { id: 4, name: 'post 4', description: 'asdasdasfhgdhsfadfgkvnrgekatgch', category: 2  },
+      { id: 5, name: 'post 5', description: 'asdasdasfhgdhsfadfgkvnrgekatgch', category: 3  },
+      { id: 6, name: 'post 6', description: 'asdasdasfhgdhsfadfgkvnrgekatgch', category: 4  },
+      { id: 7, name: 'post 7', description: 'asdasdasfhgdhsfadfgkvnrgekatgch', category: 4  },
+      { id: 8, name: 'post 8', description: 'asdasdasfhgdhsfadfgkvnrgekatgch', category: 5  },
+      { id: 9, name: 'post 9', description: 'asdasdasfhgdhsfadfgkvnrgekatgch', category: 6 },
+      { id: 10, name: 'post 10', description: 'asdasdasfhgdhsfadfgkvnrgekatgch', category: 6  }
     ];
-    return {posts};
+    const categories = [
+      { id: 1, name: 'category 1'},
+      { id: 2, name: 'category 2'},
+      { id: 3, name: 'category 3'},
+      { id: 4, name: 'category 4'},
+      { id: 5, name: 'category 5'},
+      { id: 6, name: 'category 6'}
+    ];
+    return {posts, categories};
   }
 
-  // Overrides the genId method to ensure that a hero always has an id.
-  // If the heroes array is empty,
-  // the method below returns the initial number (11).
-  // if the heroes array is not empty, the method below returns the highest
-  // hero id + 1.
+
   genId(posts: Post[]): number {
     return posts.length > 0 ? Math.max(...posts.map(post => post.id)) + 1 : 11;
   }
